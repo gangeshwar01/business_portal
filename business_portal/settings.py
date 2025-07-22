4e52138cc95dd7a451a0fea741cd22bd749f7ad6
gangeshwar01 <gangeshwarkumar001@gmail.com>
2025-07-22 18:56:43 +0545

    setting media 6

--- business_portal/settings.py
+++ business_portal/settings.py
@@ -132,10 +132,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Media files
+# Media files (user uploads)
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# Default storage for static files
+
+# Default storage for static and media files
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
