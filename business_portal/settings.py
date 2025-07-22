09a969812a1a4506ebc16c5041090d73cb97ecfe
gangeshwar01 <gangeshwarkumar001@gmail.com>
2025-07-22 18:38:21 +0545

    setting media 5

--- business_portal/settings.py
+++ business_portal/settings.py
@@ -133,8 +133,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
-MEDIA_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default storage for static files
 STORAGES = {
     "default": {
